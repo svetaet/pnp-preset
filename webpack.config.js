4097f561e1a02e37b17c05f1a8aba115a867803c
Radysh Arseniy <senyrad@gmail.com>
2020-04-03 22:15:19 +0300

    bundle: unused files warning, clean start script, inject NODE_ENV in a process context

--- webpack.config.js
+++ webpack.config.js
@@ -2,36 +2,43 @@ const path = require('path')
 const WebpackNodeExternals = require('webpack-node-externals')
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { UnusedFilesWebpackPlugin } = require('unused-files-webpack-plugin')
+const NodemonPlugin = require('nodemon-webpack-plugin')
 const PnpWebpackPlugin = require(`pnp-webpack-plugin`)
 
-module.exports = {
-	mode: 'production',
-	devtool: 'eval-source-map',
-	entry: './src/index.ts',
-	output: { path: path.resolve(__dirname, 'dist'), filename: 'index.js' },
-	stats: 'minimal',
-	target: 'node',
-	module: {
-		rules: [
-			{
-				use: ['babel-loader', { loader: 'eslint-loader', options: { fix: true } }],
-			},
+module.exports = (env, argv) => {
+	const mode = (process.env.NODE_ENV = argv.mode || 'production')
+	return {
+		mode,
+		devtool: 'eval-source-map',
+		entry: './src/index.ts',
+		output: { path: path.resolve(__dirname, 'dist'), filename: 'index.js' },
+		stats: 'minimal',
+		target: 'node',
+		module: {
+			rules: [
+				{
+					use: ['babel-loader', { loader: 'eslint-loader', options: { fix: true } }],
+				},
+			],
+		},
+		plugins: [
+			new CleanWebpackPlugin(),
+			new NodemonPlugin(),
+			new UnusedFilesWebpackPlugin({ patterns: ['src/**/*.*'] }),
+			new BundleAnalyzerPlugin({
+				analyzerMode: 'static',
+				reportFilename: path.resolve(__dirname, 'dist', 'bundle_size.html'),
+				openAnalyzer: false,
+				logLevel: 'silent',
+			}),
 		],
-	},
-	plugins: [
-		new CleanWebpackPlugin(),
-		new BundleAnalyzerPlugin({
-			analyzerMode: 'static',
-			reportFilename: path.resolve(__dirname, 'dist', 'bundle_size.html'),
-			openAnalyzer: false,
-			logLevel: 'silent',
-		}),
-	],
-	resolve: {
-		extensions: ['.ts', '.js', '.json'],
-		modules: [path.resolve(__dirname, 'src')],
-		plugins: [PnpWebpackPlugin],
-	},
-	resolveLoader: { plugins: [PnpWebpackPlugin.moduleLoader(module)] },
-	externals: [WebpackNodeExternals()],
+		resolve: {
+			extensions: ['.ts', '.js', '.json'],
+			modules: [path.resolve(__dirname, 'src')],
+			plugins: [PnpWebpackPlugin],
+		},
+		resolveLoader: { plugins: [PnpWebpackPlugin.moduleLoader(module)] },
+		externals: [WebpackNodeExternals()],
+	}
 }
